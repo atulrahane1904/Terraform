0c0a16ba768ea50d779400468d9af0da9047fc47
atulrhane <atulrahane1904@gmail.com>
2024-05-30 14:57:59 +0530

    change

--- instance1.tf
+++ instance1.tf
@@ -1,8 +1,8 @@
-provider "aws"{
+provider "aws" {
     region = "us-west-1"
 }
 resource "aws_instance" "instance"{
-ami = "ami-04b70fa74e45c3917"
-instance_type = "t2.micro"
-key_name = "mini"
+   ami = "ami-0bb84b8ffd87024d8"
+   instance_type = "t2.micro"
+   key_name = "mini"
 }
