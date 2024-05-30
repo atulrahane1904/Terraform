89acbd6937a1332007bac76167e06614abf947b6
atulrahane1904 <atulrahane1904@gmail.com>
2024-05-30 15:15:03 +0530

    Update instance1.tf

--- instance1.tf
+++ instance1.tf
@@ -1,8 +1,9 @@
 provider "aws" {
-    region = "us-west-1"
+  region = "us-west-2"
 }
-resource "aws_instance" "instance"{
-  ami = "ami-00beae93a2d981137"
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  key_name = "mini"
 }
+
